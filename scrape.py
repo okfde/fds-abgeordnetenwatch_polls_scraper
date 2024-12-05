6c7e924a4ed2b2926b3096f9ca9ee3d679a5be59
gpkvt <git@21x9.org>
2024-12-05 12:01:16 +0100

    Init

---
+++ scrape.py
@@ -0,0 +1,103 @@
+"""
+Scrape poll data from abgeordnetenwatch
+"""
+
+from pprint import pprint
+import json
+import traceback
+import requests
+
+# pylint: disable=line-too-long
+# pylint: disable=consider-using-dict-items
+# pylint: disable=bare-except
+# pylint: disable=broad-exception-caught
+# pylint: disable=invalid-name
+
+legislature = 132
+range_end = 1000
+
+URL = f"https://www.abgeordnetenwatch.de/api/v2/polls?field_legislature%5Bentity.id%5D={legislature}&range_end={range_end}"
+
+resp = requests.get(url=URL)
+polls = resp.json()
+
+if "meta" in polls:
+    print(f"Status : {polls['meta']['status']}")
+    print(f"Results: {polls['meta']['result']['count']}")
+    print("")
+
+i = 0
+err = 0
+vote_results = {}
+for poll in polls["data"]:
+    i = i + 1
+    POLL_URL = "https://www.abgeordnetenwatch.de/api/v2/polls/"+str(poll['id'])
+    params = {
+        "related_data": "votes",
+        "range_end": 1
+    }
+    poll_resp = requests.get(url=POLL_URL, params=params)
+    poll_data = poll_resp.json()
+
+    if range_end < polls['meta']['result']['count']:
+        print("WARNING: Got more results than range_end, results will be incomplete!")
+
+    try:
+        print(f"Request    : {i}/{polls['meta']['result']['count']}")
+        print(f"API call   : {poll_resp.url}")
+        print(f"Poll       : {poll['label']}")
+        print(f"URL        : {poll['abgeordnetenwatch_url']}")
+        print(f"Legislature: {poll['field_legislature']['label']}")
+        print(f"Date       : {poll['field_poll_date']}")
+
+        if not poll["label"] in vote_results:
+            vote_results[poll["label"]] = {}
+            vote_results[poll["label"]]["meta"] = {
+                "id": i,
+                "url": poll['abgeordnetenwatch_url']
+            }
+
+        for vote in poll_data["data"]["related_data"]["votes"]:
+            if not "label" in vote["fraction"]:
+                vote["fraction"] = {
+                    "label": "Unknown"
+                }
+            if not vote["fraction"]["label"] in vote_results[poll["label"]]:
+                vote_results[poll["label"]][vote["fraction"]["label"]] = {
+                    "no_show": 0,
+                    "yes": 0,
+                    "no": 0,
+                    "abstain": 0
+                }
+
+            vote_results[poll["label"]][vote["fraction"]["label"]][vote["vote"]] = vote_results[poll["label"]][vote["fraction"]["label"]][vote["vote"]] + 1
+    except Exception as e:
+        err = err + 1
+        print(f"Could not fully parse poll {i}")
+        print(traceback.format_exc())
+
+    print("")
+
+if err > 0:
+    print(f"{err} polls could not be fully parsed")
+
+# json
+with open("votes.json", "w", encoding="utf-8") as f:
+    json.dump(vote_results, f)
+f.close()
+
+# csv
+csv_lines = []
+csv_lines.append("id, vote, fraction, no_show, yes, no, abstain, URL")
+for vote in vote_results:
+    for fraction in vote_results[vote]:
+        if fraction == "meta":
+            continue
+        vote_name = vote.replace("\"", "'")
+        csv_lines.append(f'{vote_results[vote]["meta"]["id"]}, "{vote_name}", "{fraction}", "{vote_results[vote][fraction]["no_show"]}", "{vote_results[vote][fraction]["yes"]}", "{vote_results[vote][fraction]["no"]}", "{vote_results[vote][fraction]["abstain"]}", {vote_results[vote]["meta"]["url"]}')
+
+with open("votes.csv", "w", encoding="utf-8") as f:
+    for line in csv_lines:
+        f.write(line)
+        f.write("\n")
+f.close()
